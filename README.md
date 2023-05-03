a131bd72094473dc1dadc86b87beaf6fbeae361e
Antun Matija Filipović <a.m.filipovic@gmail.com>
2023-05-03 17:25:11 +0200

    master

---
+++ README.md
@@ -0,0 +1,17 @@
+# encoding_tools
+
+A collection of tools developed to facilitate the encoding and decoding of data through multiple methods and techniques.
+
+The purpose of this repository is to provide a set of tools for encoding and decoding various forms of data, utilizing an array of techniques and algorithms, streamlining the workflow and enhancing the overall efficiency of data processing.
+
+## Author
+
+Antun Matija Filipović | [@amfilipovic](https://github.com/amfilipovic)
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/license/mit/) - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Contributing
+
+If you find a bug or have an idea for a new feature or improvement, please open an issue or you can submit a pull request. Contributions and ideas are always welcome!
---
+++ ascii_encoder_cli.py
@@ -0,0 +1,25 @@
+import os
+
+# Clear the screen.
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# Ask the user to input text to encode.
+text = input("Input text to encode: ")
+
+# Encode the inputted text using ASCII decimal encoding.
+ascii_dec_enc_text = ''.join(str(ord(c)) for c in text)
+
+# Encode the inputted text using ASCII hexadecimal encoding.
+ascii_hex_enc_text = ''.join(hex(ord(c))[2:] for c in text).upper()
+
+# Encode the inputted text using ASCII octal encoding.
+ascii_oct_enc_text = ''.join(oct(ord(c))[2:] for c in text)
+
+# Encode the inputted text using ASCII binary encoding.
+ascii_bin_enc_text = ''.join(format(ord(c), '08b') for c in text)
+
+# Print the ASCII decimal, hexadecimal, octal and binary encoded text.
+print(f"ASCII decimal encoded text: {ascii_dec_enc_text}")
+print(f"ASCII hexadecimal encoded text: {ascii_hex_enc_text}")
+print(f"ASCII octal encoded text: {ascii_oct_enc_text}")
+print(f"ASCII binary encoded text: {ascii_bin_enc_text}")
