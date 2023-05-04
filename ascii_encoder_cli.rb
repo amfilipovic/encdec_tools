46ab25e84ddce42ee3aa206ad41d34d5478c2c7d
Antun Matija Filipović <a.m.filipovic@gmail.com>
2023-05-04 20:58:24 +0200

    master

--- ascii_encoder_cli.rb
+++ ascii_encoder_cli.rb
@@ -6,16 +6,16 @@ print "Input text to encode: "
 text = gets.chomp
 
 # Encode the inputted text using ASCII decimal encoding.
-ascii_dec_enc_text = text.chars.map(&:ord).join
+ascii_dec_enc_text = text.chars.map(&:ord).join(' ')
 
 # Encode the inputted text using ASCII hexadecimal encoding.
-ascii_hex_enc_text = text.chars.map { |c| c.ord.to_s(16) }.join.upcase
+ascii_hex_enc_text = text.chars.map { |c| c.ord.to_s(16) }.join(' ').upcase
 
 # Encode the inputted text using ASCII octal encoding.
-ascii_oct_enc_text = text.chars.map { |c| c.ord.to_s(8) }.join
+ascii_oct_enc_text = text.chars.map { |c| c.ord.to_s(8) }.join(' ')
 
 # Encode the inputted text using ASCII binary encoding.
-ascii_bin_enc_text = text.chars.map { |c| c.ord.to_s(2).rjust(8, "0") }.join
+ascii_bin_enc_text = text.chars.map { |c| c.ord.to_s(2).rjust(8, "0") }.join(' ')
 
 # Print the ASCII decimal, hexadecimal, octal and binary encoded text.
 puts "ASCII decimal encoded text: #{ascii_dec_enc_text}"
--- unicode_encoder_cli.rb
+++ unicode_encoder_cli.rb
@@ -6,7 +6,7 @@ print "Input text to encode: "
 text = gets.chomp
 
 # Encode the inputted text using Unicode encoding.
-unicode_enc_text = text.each_codepoint.map { |cp| "\\u#{cp.to_s(16).rjust(4, '0')}" }.join
+unicode_enc_text = text.each_codepoint.map { |cp| "\\u#{cp.to_s(16).rjust(4, '0')}" }.join(' ')
 
 # Print the Unicode encoded text.
 puts "Unicode encoded text: #{unicode_enc_text}"
