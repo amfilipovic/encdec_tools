959c309b729405bba88971ac53c6b8357b721f51
Antun Matija Filipović <a.m.filipovic@gmail.com>
2023-05-05 03:00:30 +0200

    master

--- ascii_encoder_cli.c
+++ ascii_encoder_cli.c
@@ -22,33 +22,35 @@ int main() {
     int i;
     int jdec = 0;
     for (i = 0; i < strlen(text); i++) {
-        jdec += sprintf(&ascii_dec_enc_text[jdec], "%d", text[i]);
+        jdec += sprintf(&ascii_dec_enc_text[jdec], "%d ", text[i]);
     }
-    ascii_dec_enc_text[jdec] = '\0';
+    ascii_dec_enc_text[jdec-1] = '\0';
     // Encode the inputted text using ASCII hexadecimal encoding.
     char ascii_hex_enc_text[100];
     for (i = 0; i < strlen(text); i++) {
-    sprintf(&ascii_hex_enc_text[i*2], "%02X", text[i]);
+    sprintf(&ascii_hex_enc_text[i*3], "%02X ", text[i]);
     }
-    ascii_hex_enc_text[strlen(text)*2] = '\0';
+    ascii_hex_enc_text[strlen(text)*3-1] = '\0';
     // Encode the inputted text using ASCII octal encoding.
     char ascii_oct_enc_text[100];
     int joct = 0;
     joct = 0;
     for (i = 0; i < strlen(text); i++) {
-        sprintf(&ascii_oct_enc_text[joct], "%o", (unsigned char) text[i]);
+        sprintf(&ascii_oct_enc_text[joct], "%o ", (unsigned char) text[i]);
         joct += strlen(&ascii_oct_enc_text[joct]);
     }
+    ascii_oct_enc_text[joct-1] = '\0';
     // Encode the inputted text using ASCII binary encoding.
     char ascii_bin_enc_text[100];
     for (i = 0; i < strlen(text); i++) {
         int num = text[i];
         for (int jbin = 7; jbin >= 0; jbin--) {
             int bit = (num >> jbin) & 1;
-            ascii_bin_enc_text[i*8 + (7-jbin)] = bit ? '1' : '0';
+            ascii_bin_enc_text[i*9 + (7-jbin)] = bit ? '1' : '0';
         }
+        ascii_bin_enc_text[i*9 + 8] = ' ';
     }
-    ascii_bin_enc_text[strlen(text)*8] = '\0';
+    ascii_bin_enc_text[strlen(text)*9-1] = '\0';
 // Print the ASCII decimal, hexadecimal, octal and binary encoded text.
 printf("ASCII decimal encoded text: %s\n", ascii_dec_enc_text);
 printf("ASCII hexadecimal encoded text: %s\n", ascii_hex_enc_text);
